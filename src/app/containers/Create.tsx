e22caa921ee4867d36a8fa3d586a82d4a89577dc
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-03-31 20:56:20 +0430

    Typography

--- src/app/containers/Create.tsx
+++ src/app/containers/Create.tsx
@@ -1,5 +1,18 @@
+import { Typography } from "@material-ui/core";
+
 function Create() {
-  return <div>Create page</div>;
+  return (
+    <div>
+      <Typography
+        color="textSecondary"
+        variant="h6"
+        component="h2"
+        gutterBottom
+      >
+        Create a New Note
+      </Typography>
+    </div>
+  );
 }
 
 export default Create;
