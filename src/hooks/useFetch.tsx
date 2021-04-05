7267ce08d89e783291aff9a46ac60c639ed1ea2e
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-04-05 18:05:53 +0430

    **** use axios.CancelToken and cleanup function(unmount) and implement updateResponse function  ****

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,32 +1,48 @@
-import { AxiosRequestConfig } from "axios";
+import axios, { AxiosRequestConfig } from "axios";
 import { useEffect, useState } from "react";
+import { NoteType } from "../app/containers/types";
 import axiosIntance from "../services/base";
 
 type AxiosOptions = Omit<AxiosRequestConfig, "baseURL" | "url">;
 
 function useFetch(url: string, options?: AxiosOptions) {
-  const [response, setResponse] = useState([]);
+  const [response, setResponse] = useState<NoteType[]>([]);
   const [error, setError] = useState("");
 
   useEffect(() => {
+    const source = axios.CancelToken.source();
+
     const fetchData = async () => {
       try {
         const res = await axiosIntance.request({
           url,
+          cancelToken: source.token,
           ...options,
         });
+
         setResponse(res.data);
       } catch (e) {
-        setError(e.message);
+        if (axios.isCancel(e)) {
+        } else {
+          setError(e.message);
+        }
       }
     };
-
     fetchData();
+
+    // unmount
+    return () => {
+      source.cancel();
+    };
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  return { response, error };
+  const updateResponse = (newNote: NoteType[]) => {
+    setResponse(newNote);
+  };
+
+  return { response, updateResponse, error };
 }
 
 export default useFetch;
