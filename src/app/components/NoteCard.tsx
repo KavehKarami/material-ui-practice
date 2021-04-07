e72f49cf405cdc1c137018a9230397a89040bd02
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-04-07 19:57:35 +0430

    add avatar to NoteCard

--- src/app/components/NoteCard.tsx
+++ src/app/components/NoteCard.tsx
@@ -1,19 +1,29 @@
 import {
+  Avatar,
   Card,
   CardContent,
   CardHeader,
   IconButton,
+  makeStyles,
   Typography,
 } from "@material-ui/core";
+import { green, lightBlue, pink, yellow } from "@material-ui/core/colors";
 import DeleteOutlinedIcon from "@material-ui/icons/DeleteOutlined";
 import { NoteCardProps } from "./types";
 
 function NoteCard({ note, handleDelete }: NoteCardProps) {
+  const classes = useStyles({ category: note.category });
+
   return (
     <Card elevation={1}>
       <CardHeader
         title={note.title}
         subheader={note.category}
+        avatar={
+          <Avatar className={classes.avatar}>
+            {note.category[0].toUpperCase()}
+          </Avatar>
+        }
         action={
           <IconButton onClick={() => handleDelete(note.id)}>
             <DeleteOutlinedIcon />
@@ -31,3 +41,14 @@ function NoteCard({ note, handleDelete }: NoteCardProps) {
 }
 
 export default NoteCard;
+
+const useStyles = makeStyles({
+  avatar: {
+    backgroundColor: ({ category }: { category: string }) => {
+      if (category === "work") return yellow[700];
+      if (category === "money") return green[500];
+      if (category === "todos") return lightBlue[500];
+      return pink[500];
+    },
+  },
+});
