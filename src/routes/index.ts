e8c0d7032c147c5fbf26c073cc6d902a8fe022f7
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-04-07 17:20:07 +0430

    change route and add icon into that

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,25 @@
 import { Routes } from "./types";
 import Notes from "../app/containers/Notes";
 import Create from "../app/containers/Create";
+import SubjectIcon from "@material-ui/icons/Subject";
+import AddCircleOutlineIcon from "@material-ui/icons/AddCircleOutline";
 
 const routes: Routes[] = [
   {
     component: Notes,
     path: "/",
     exact: true,
-    name: "Notes",
+    name: "My Notes",
     isMenu: true,
+    icon: SubjectIcon,
   },
   {
     component: Create,
     path: "/create",
     exact: true,
-    name: "Create",
+    name: "Create Note",
     isMenu: true,
+    icon: AddCircleOutlineIcon,
   },
 ];
 
--- src/routes/types.ts
+++ src/routes/types.ts
@@ -1,7 +1,11 @@
+import { SvgIconTypeMap } from "@material-ui/core";
+import { OverridableComponent } from "@material-ui/core/OverridableComponent";
+
 export interface Routes {
   component: () => JSX.Element;
   path: string;
-  exact?: boolean;
+  exact: boolean;
   name: string;
   isMenu: boolean;
+  icon: OverridableComponent<SvgIconTypeMap<{}, "svg">>;
 }
