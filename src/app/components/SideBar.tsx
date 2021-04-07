c110f7804a0b6a3813482e4bc0f2c79dba74b830
Kaveh Karami <kaveh.karami.kk@gmail.com>
2021-04-07 17:25:48 +0430

    List and ListItem

--- src/app/components/SideBar.tsx
+++ src/app/components/SideBar.tsx
@@ -1,8 +1,19 @@
-import { Drawer, Typography } from "@material-ui/core";
+import {
+  Drawer,
+  List,
+  ListItem,
+  ListItemIcon,
+  ListItemText,
+  Typography,
+} from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
+import { useHistory, useLocation } from "react-router";
+import routes from "../../routes";
 
 function SideBar() {
   const classes = useStyles();
+  const history = useHistory();
+  const location = useLocation();
 
   return (
     <Drawer
@@ -13,6 +24,25 @@ function SideBar() {
       <div>
         <Typography variant="h5">Kavehhhhh</Typography>
       </div>
+
+      <List>
+        {routes.map(
+          (route) =>
+            route.isMenu && (
+              <ListItem
+                key={route.path}
+                onClick={() => history.push(route.path)}
+                className={
+                  location.pathname === route.path ? classes.active : undefined
+                }
+                button
+              >
+                <ListItemIcon>{<route.icon color="secondary" />}</ListItemIcon>
+                <ListItemText primary={route.name} />
+              </ListItem>
+            )
+        )}
+      </List>
     </Drawer>
   );
 }
@@ -26,5 +56,17 @@ const useStyles = makeStyles({
   },
   drawerPaper: {
     width: drawerWith,
+  },
+  navLink: {
+    backgroundColor: "#fff",
+    display: "block",
+    textDecoration: "none",
+
+    "&:hover, &:focus": {
+      backgroundColor: "transparent",
+    },
+  },
+  active: {
+    backgroundColor: "#f4f4f4 !important",
   },
 });
